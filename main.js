a35b08790f0ad5a602063609278b140dff047efd
nadiairina <nadiairina934@gmail.com>
2026-03-11 21:32:28 +0000

    Update main.js

--- main.js
+++ main.js
@@ -74,7 +74,7 @@ document.addEventListener('DOMContentLoaded', () => {
             stars: "★★★★☆",
             quote: "Ninguém sabe o que acontece atrás de portas fechadas.",
             text: "Um thriller psicológico viciante e claustrofóbico. Freida McFadden constrói uma teia de mentiras que nos mantém a adivinhar até à última página."
-        
+        }
     };
 
     // Função para fechar a modal de forma limpa
